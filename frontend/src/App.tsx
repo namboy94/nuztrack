ace0fb50a6bc1a485dbbe45d5a4f3b2bb5b190ad
Hermann Krumrey <hermann@krumreyh.com>
2022-05-21 13:27:24 +0200

    Create simple interactions between frontend and backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "@mui/material";
+import axios from "axios";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button onClick={testCall} variant={"contained"}>Hello World</Button>
   );
+}
+
+function testCall() {
+  axios.defaults.baseURL = "http://0.0.0.0:8080"
+  axios.get("/api/test")
+  axios.get("/api/count").then((resp) => window.alert(resp.data))
 }
 
 export default App;
