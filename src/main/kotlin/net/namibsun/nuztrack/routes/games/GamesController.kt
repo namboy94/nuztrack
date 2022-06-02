f7b4d490ffe08339af6eea3114505257b6f67330
Hermann Krumrey <hermann@krumreyh.com>
2022-06-02 20:48:39 +0200

    Change /games API endpoint to be a raw dictionary

--- src/main/kotlin/net/namibsun/nuztrack/routes/games/GamesController.kt
+++ src/main/kotlin/net/namibsun/nuztrack/routes/games/GamesController.kt
@@ -1,6 +1,6 @@
 package net.namibsun.nuztrack.routes.games
 
-import net.namibsun.nuztrack.util.Games
+import net.namibsun.nuztrack.constants.Games
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -11,8 +11,8 @@ class GamesController {
 
     @GetMapping("/games")
     @ResponseBody
-    fun getGames(): ResponseEntity<GamesListTO> {
-        val gamesList = GamesListTO(Games.values().associate { it.name.lowercase() to it.title })
+    fun getGames(): ResponseEntity<Map<String, String>> {
+        val gamesList = Games.values().associate { it.name.lowercase() to it.title }
         return ResponseEntity.ok(gamesList)
     }
 }
