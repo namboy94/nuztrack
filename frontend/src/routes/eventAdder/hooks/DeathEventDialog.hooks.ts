2bed22d5442a80c3d2667b645be741e047f3e118
Hermann Krumrey <hermann@krumreyh.com>
2022-08-06 11:36:33 +0200

    WIP Rewriting of DeathEventDialog hook to use ViewModel

--- frontend/src/routes/eventAdder/hooks/DeathEventDialog.hooks.ts
+++ frontend/src/routes/eventAdder/hooks/DeathEventDialog.hooks.ts
@@ -1,7 +1,5 @@
 import {NuzlockeRun} from "../../../data/runs/runs.model";
 import {NotificationFN} from "../../../global/Snackbar";
-import {DeathEventDialogProps, DeathEventDialogState} from "../components/DeathEventDialog";
-import {CreateDeathEvent} from "../../../data/events/events.model";
 import {useQuery, useSubmitter} from "../../../util/hooks/observable";
 import {eventsService} from "../../../data/events/events.service";
 import {useState} from "react";
@@ -10,43 +8,46 @@ import {gamesService} from "../../../data/games/games.service";
 import {teamService} from "../../../data/team/team.service";
 import {useResetState} from "../../../util/hooks/state";
 import {TeamMember} from "../../../data/team/team.model";
+import {ViewModel} from "../../../util/viewmodel";
+import {Pokedex} from "../../../data/pokedex/pokedex.model";
+import {GameLocation} from "../../../data/games/games.model";
 
-export function useDeathEventDialogProps(
-    run: NuzlockeRun,
-    notify: NotificationFN
-): [() => void, DeathEventDialogProps] {
+export interface DeathEventDialogState {
+    open: boolean
+    pokedex: Pokedex
+    locations: GameLocation[]
+    activeTeamMembers: TeamMember[]
+    boxedTeamMembers: TeamMember[]
+    location: string
+    level: number
+    teamMember: TeamMember | null
+    opponent: string
+    description: string
+}
 
-    const [open, setOpen] = useState(false)
+export interface DeathEventDialogInteractions {
+    openDialog: () => void
+    onClose: () => void
+    onChangeLocation: (location: string) => void
+    onChangeLevel: (level: number) => void
+    onChangeTeamMember: (teamMember: TeamMember | null) => void
+    onChangeOpponent: (opponent: string) => void
+    onChangeDescription: (description: string) => void
+    submit: () => void
+}
+
+
+export type DeathEventDialogViewModel = ViewModel<DeathEventDialogState, DeathEventDialogInteractions>
+
+export function useDeathEventDialogProps(run: NuzlockeRun, notify: NotificationFN): DeathEventDialogViewModel {
     const pokedex = useQuery(() => pokedexService.getPokedex$(), undefined, [])
-    const locationRegistry = useQuery(() => gamesService.getGameLocationRegistry$(run.game), undefined, [])
-    const locations = locationRegistry?.getLocationNames() ?? []
+    const locations = useQuery(
+        () => gamesService.getGameLocationRegistry$(run.game), undefined, []
+    )?.getLocationNames() ?? []
     const activeTeamMembers = useQuery(() => teamService.getActiveTeamMembers$(run.id), [], [])
     const boxedTeamMembers = useQuery(() => teamService.getBoxedTeamMembers$(run.id), [], [])
 
-    const state = useDeathEventDialogState()
-
-    const onClose = () => {
-        setOpen(false)
-        state.reset()
-    }
-
-    const submit = useDeathEventSubmit(run, notify, onClose, state)
-
-    const props: DeathEventDialogProps = {
-        activeTeamMembers: activeTeamMembers,
-        boxedTeamMembers: boxedTeamMembers,
-        locations: locations,
-        onClose: onClose,
-        open: open,
-        pokedex: pokedex,
-        state: state,
-        submit: submit
-    }
-    return [() => setOpen(true), props]
-
-}
-
-function useDeathEventDialogState(): DeathEventDialogState {
+    const [open, setOpen] = useState(false)
     const [location, setLocation, resetLocation] = useResetState("")
     const [teamMember, setTeamMember, resetTeamMember] = useResetState<TeamMember | null>(null)
     const [level, setLevel, resetLevel] = useResetState(5)
@@ -61,50 +62,55 @@ function useDeathEventDialogState(): DeathEventDialogState {
         resetDescription()
     }
 
-    const selectTeamMember = (teamMember: TeamMember | null) => {
+    const onChangeTeamMember = (teamMember: TeamMember | null) => {
         setTeamMember(teamMember)
         setLevel(teamMember?.level ?? 5)
     }
 
-    return {
-        teamMember: teamMember,
-        level: level,
-        location: location,
-        description: description,
-        opponent: opponent,
-        reset: reset,
-        setLevel: setLevel,
-        setLocation: setLocation,
-        setTeamMember: selectTeamMember,
-        setDescription: setDescription,
-        setOpponent: setOpponent
+    const openDialog = () => setOpen(true)
+
+    const onClose = () => {
+        setOpen(false)
+        reset()
     }
 
-}
-
-function useDeathEventSubmit(
-    run: NuzlockeRun,
-    notify: NotificationFN,
-    onClose: () => void,
-    state: DeathEventDialogState
-): () => void {
-    const creator: CreateDeathEvent = {
-        description: state.description,
-        level: state.level,
-        location: state.location,
-        opponent: state.opponent,
-        teamMemberId: state.teamMember?.id ?? -1
-    }
-
-    const onSuccess = () => {
+    const onSubmitSuccess = () => {
         onClose()
         notify("Successfully created Death Event", "success")
     }
 
-    const onError = (e: any) => {
-        notify(`Failed to create Death Event: '${e.response.data.reason}'`, "error")
+    const onSubmitError = (e: any) => notify(`Failed to create Death Event: '${e.response.data.reason}'`, "error")
+
+    const submit = useSubmitter(() => eventsService.createDeathEvent$(run.id, {
+        description: description,
+        level: level,
+        location: location,
+        opponent: opponent,
+        teamMemberId: teamMember?.id ?? -1
+    }), onSubmitSuccess, onSubmitError)
+
+    return {
+        state: {
+            open: open,
+            pokedex: pokedex,
+            locations: locations,
+            activeTeamMembers: activeTeamMembers,
+            boxedTeamMembers: boxedTeamMembers,
+            location: location,
+            level: level,
+            teamMember: teamMember,
+            opponent: opponent,
+            description: description
+        },
+        interactions: {
+            openDialog: openDialog,
+            onClose: onClose,
+            onChangeDescription: setDescription,
+            onChangeLevel: setLevel,
+            onChangeLocation: setLocation,
+            onChangeOpponent: setOpponent,
+            onChangeTeamMember: onChangeTeamMember,
+            submit: submit
+        }
     }
-
-    return useSubmitter(() => eventsService.createDeathEvent$(run.id, creator), onSuccess, onError)
-
 }
